655a4ca9366c0b456d0bf9cfa6b492886885a88f
Isabela <isa.bela.160703@gmail.com>
2026-06-23 16:58:02 -0500

    calculo de fuerzas gravitacionales

--- physics.py
+++ physics.py
@@ -1,18 +1,17 @@
 import numpy as np
-import multiprocessing as mp
-from physics import G, SOFTENING
 
-def worker_compute_forces(args):
-    positions, masses, start_idx, end_idx = args
-    chunk_forces = np.zeros((end_idx - start_idx, 2))
+G = 0.5  
+SOFTENING = 5.0  
 
-    for idx, i in enumerate(range(start_idx, end_idx)):
+def compute_forces(positions, masses):
+    n = len(positions)
+    forces = np.zeros((n, 2))
+    for i in range(n):
         diff = positions - positions[i]
         dist_sq = np.sum(diff**2, axis=1) + SOFTENING**2
         force_mag = G * masses[i] * masses[:, 0] / dist_sq
-
+        
         dist = np.sqrt(dist_sq)
         direction = diff / dist[:, np.newaxis]
-        chunk_forces[idx] = np.sum(direction * force_mag[:, np.newaxis], axis=0)
-
-    return chunk_forces
+        forces[i] = np.sum(direction * force_mag[:, np.newaxis], axis=0)
+    return forces
